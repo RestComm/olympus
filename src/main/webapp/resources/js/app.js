030a845e9dc5c1cff65c2d7a08f9d84829de069e
Antonis Tsakiridis <antonis.tsakiridis@telestax.com>
2018-09-27 16:56:20 +0300

    FS-453: Whitelabel Olympus

--- src/main/webapp/resources/js/app.js
+++ src/main/webapp/resources/js/app.js
@@ -29,3 +29,74 @@ olyMod.config(['$routeProvider', /*'$locationProvider',*/ function($routeProvide
     when('/home', {templateUrl: 'modules/home.html', controller: 'HomeCtrl'}).
     otherwise({redirectTo: '/'});
 }]);
+
+let vndId = window.location.hostname.substr(0, window.location.hostname.indexOf('.')) || window.location.hostname;
+
+angular.element(document).ready(['$http', function ($http) {
+  // manually inject $q since it's not available
+  var initInjector = angular.injector(['ng']);
+  var $q = initInjector.get('$q');
+  var $window = initInjector.get('$window');
+
+/*
+  var configPromise = $q.defer();
+  $http.get($window.location.pathname + 'conf/dashboard.json?v=' + (+new Date())).success(function (response) {
+    angular.module('rcApp.services').factory('PublicConfig', function () {
+      var pConfig = typeof response === 'string' ? JSON.parse(response) : response;
+      // initialize countly key, if provided
+      loadCountly(pConfig.countlyKey);
+      return pConfig;
+    });
+    configPromise.resolve(response);
+  }).error(function () {
+    configPromise.reject();
+  });
+*/
+
+  let wlDefaultPromise = $q.defer();
+  $http.get($window.location.pathname + 'conf/settings.json?v=' + (+new Date())).success((response) => {
+    wlDefaultPromise.resolve(response);
+  }).error(() => {
+    wlDefaultPromise.resolve({});
+  });
+
+  let wlPromise = $q.defer();
+  $http.get($window.location.pathname + `resources/vnd/${vndId}/settings.json?v=` + (+new Date())).success((response) => {
+    wlPromise.resolve(response);
+  }).error(() => {
+    wlPromise.resolve({});
+  });
+
+  $q.all([wlDefaultPromise.promise, wlPromise.promise]).then((responses) => {
+    angular.module('mcWebRTC.services').factory('WLSettings', function () {
+      let defSettings = typeof responses[0] === 'string' ? JSON.parse(responses[0]) : responses[0];
+      let wlSettings = typeof responses[1] === 'string' ? JSON.parse(responses[1]) : responses[1];
+      return _.extend({}, defSettings, wlSettings);
+    });
+    angular.bootstrap(document, ['mcWebRTC']);
+  }, (error) => {
+    console.error('Internal server error', error);
+  });
+}]);
+
+olyMod.run(function($rootScope, $location, $anchorScroll, $http, WLSettings) {
+
+  $rootScope.vndId = vndId;
+  var defaultFaviconUrl = 'resources/images/favicon.png';
+  var vendorFaviconUrl = 'resources/vnd/' + $rootScope.vndId + '/favicon.png';
+
+  $http({method: 'GET', url: vendorFaviconUrl}).then(
+    function(response) {
+      if (response.status === 200) {
+        // there's some caching issue if the filename is same as already used, append timestamp
+        $rootScope.faviconUrl = vendorFaviconUrl + '?' + Date.now();
+      } else {
+        $rootScope.faviconUrl = defaultFaviconUrl;
+      }
+    },
+    function () {
+      $rootScope.faviconUrl = defaultFaviconUrl;
+    });
+
+  $rootScope.wls = WLSettings;
+});
